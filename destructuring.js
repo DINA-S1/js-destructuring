89f7bdaa6f77cbe15de25f1876f683f36225473f
DINA-S1 <dina.steenvoorden@gmail.com>
2021-07-08 15:03:34 +0200

    opdracht af

--- destructuring.js
+++ destructuring.js
@@ -6,6 +6,10 @@ const product = {
   screenType: 'LED',
 }
 
+const { price, refreshRate, screenType  } = product
+
+console.log( product )
+
 // Opdracht 2: Destructure type en brand uit onderstaand object, en log ze daarna in de terminal
 
 const productInformation = {
@@ -17,6 +21,13 @@ const productInformation = {
   screenQuality: 'Ultra HD/4K',
   smartTv: true,
 }
+
+const type = productInformation.general.type
+const brand = productInformation.general.brand
+
+console.log( type )
+console.log( brand )
+
 
 // Opdracht 3: destructure wifi en bluetooth uit onderstaand object, en log ze daarna in de terminal
 
@@ -36,17 +47,31 @@ const tvOptions = {
       infrared: false,
     }
   },
-};
+}
+
+const wifi = tvOptions.options.connectivity.popular.wifi
+const bluetooth = tvOptions.options.connectivity.popular.bluetooth
+
+console.log( wifi )
+console.log( bluetooth )
+
 
 // Opdracht 4: Destructure beide waardes uit onderstaande array, en log ze daarna in de terminal
 
-const connectivity = ['wifi', 'bluetooth'];
+const connectivity = ['wifi', 'bluetooth']
+const[ first, second ] = connectivity
 
+
+console.log( first )
+console.log( second )
 
 // Opdracht 5: Destructure de >eerste< en >derde< waarde uit onderstaande array, en log ze daarna in de terminal
 
 const tvSizes = [41, 43, 46, 56, 65];
+const[ eerste,, derde, ...rest] = tvSizes
 
+console.log( eerste )
+console.log( derde )
 
 // opdracht 6: Destructure name en adress uit de return value van deze functie en log ze daarna in de terminal
 
@@ -57,8 +82,17 @@ function getCompanyDetails() {
   }
 }
 
+const { name, adress } = getCompanyDetails()
+console.log( name )
+console.log( adress )
+
+
 // opdracht 7: Destructure beide zinnetjes uit de return value van de getDetails functie en log ze in de terminal. Let op: de functie verwacht argumenten bij het aanroepen!
 
 function getDetails(name, age) {
   return [`Your name is ${name}`, `You are ${age} years old`];
 }
+
+
+
+console.log( getDetails( "Jan", "30" ))
